26a36f55b86990b02e1689665cf98abb5d2cf256
Shasaang <53186606+Shasaang@users.noreply.github.com>
2019-09-29 21:03:19 +0530

    Add files via upload

---
+++ test1.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param( $appname,
+       $aspname,
+       $rgname
+)
+
+$dyobject = @{
+ 'webAppName' = $appname
+ 'appServicePlanName' = $aspname
+ 'Location' = 'Central US'
+ 'StorageTier' =  'S1'
+}
+
+$rg=$rgname
+$loc="Central US"
+
+$password = ConvertTo-SecureString 'AzureTest@123' -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ('deepak@sindhus.in', $password)
+
+#login to subscrption
+login-azureRmAccount -Credential $Credential
+Select-AzureRmSubscription -SubscriptionId 83f64421-ba30-4f5c-974e-c09331cf25b9
+
+#create resource group
+New-AzurermResourceGroup -Name $rg -Location $loc
+
+# Create an App Service plan and App Service in Free tier.
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateUri https://raw.githubusercontent.com/Shasaang/azuredevops/master/sha.json -TemplateParameterObject $dyobject
